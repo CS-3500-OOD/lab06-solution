c4f63966bd63f06d5c73cc8f6cb8f381f61b51e7
Mark Fontenot <mfonten@lyle.smu.edu>
2023-05-06 08:18:31 -0400

    Updated build with timeout and to add JavaDocs artifact upload

--- src/test/java/cs3500/template/code/GradeBookTest.java
+++ src/test/java/cs3500/template/code/GradeBookTest.java
@@ -1,10 +1,9 @@
 package cs3500.template.code;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static uk.org.webcompere.systemstubs.SystemStubs.tapSystemOut;
 
 import org.junit.jupiter.api.Test;
-
-
 
 class GradeBookTest {
 
@@ -13,7 +12,18 @@ class GradeBookTest {
     final double delta = 0.01;
     GradeBook g1 = new GradeBook(0, 0, 0);
     assertEquals(0, g1.getAverage(), delta);
+  }
+
+  @Test
+  public void checkSystemOutWrite() throws Exception {
+    String text = tapSystemOut(() -> {
+      printError("problem!");
+    }).trim();
+    assertEquals("problem!", text);
 
   }
 
+  private void printError(String output) {
+    System.out.println(output);
+  }
 }
